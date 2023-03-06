f608a5a80d77bff595e575c27858230a79a0f1f0
Kiprich <davidkipkemoi30@gmail.com>
2023-03-07 01:58:25 +0300

    PizzaAPI

---
+++ Test.API/Controllers/PizzaController.cs
@@ -0,0 +1,69 @@
+using Test.API.Models;
+using Test.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    [HttpGet (Name = "PIzza")]
+    public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
+
+   
+    [HttpGet("{id}")]
+public ActionResult<Pizza> Get(int id)
+{
+    var pizza = PizzaService.Get(id);
+
+    if(pizza == null)
+        return NotFound();
+
+    return pizza;
+}
+
+    [HttpPost]
+public IActionResult Create(Pizza pizza)
+{            
+    // This code will save the pizza and return a result
+    PizzaService.Add(pizza);
+    return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    
+}
+
+   [HttpPut("{id}")]
+public IActionResult Update(int id, Pizza pizza)
+{
+    // This code will update the pizza and return a result
+     if (id != pizza.Id)
+        return BadRequest();
+           
+    var existingPizza = PizzaService.Get(id);
+    if(existingPizza is null)
+        return NotFound();
+   
+    PizzaService.Update(pizza);           
+   
+    return NoContent();
+}
+
+    [HttpDelete("{id}")]
+public IActionResult Delete(int id)
+{
+    // This code will delete the pizza and return a result
+    var pizza = PizzaService.Get(id);
+   
+    if (pizza is null)
+        return NotFound();
+       
+    PizzaService.Delete(id);
+   
+    return NoContent();
+}
+}
